dd9a972fedcb9b21a42867b0d56cc7f5d164ce8e
Matt <solomatias321@gmail.com>
2024-03-02 14:59:24 +0100

    first commit

---
+++ ProductMicroservice/Program.cs
@@ -0,0 +1,41 @@
+using ProductMicroservice.Data;
+using ProductMicroservice.Services.Interface;
+using ProductMicroservice.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddDbContext<DbContextClass>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.Logger.LogInformation("Entering ..............");
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors(builder =>
+    {
+        builder.AllowAnyOrigin();
+        builder.AllowAnyMethod();
+        builder.AllowAnyHeader();
+    });
+//}
+app.Logger.LogInformation("After,,,,,,,,, ");
+
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
