57faada3d82453026aeea764631a7f5232a429aa
Matt <solomatias321@gmail.com>
2024-03-04 16:44:50 +0100

    set up ocelot

--- OcelotApiGateway/Program.cs
+++ OcelotApiGateway/Program.cs
@@ -1,14 +1,17 @@
-using UserMicroservice.Services.Interface;
-using UserMicroservice.Services;
-using UserMicroservice.Data;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddDbContext<DbUserContext>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORSPolicy", builder => builder.AllowAnyMethod().AllowAnyHeader().AllowCredentials().SetIsOriginAllowed((hosts) => true));
+});
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,18 +23,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.UseCors(builder =>
-    {
-        builder.AllowAnyOrigin();
-        builder.AllowAnyMethod();
-        builder.AllowAnyHeader();
-    });
 }
 
+app.UseCors("CORSPolicy");
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
+await app.UseOcelot();
 app.Run();
+
